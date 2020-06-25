f390fb168b57c673e6235e0d2bdecfd55a0d10d2
MongoCaleb <caleb.thompson@mongodb.com>
2020-06-25 11:32:56 -0700

    remove user prompts; add comments

--- AtlasStarter/Program.cs
+++ AtlasStarter/Program.cs
@@ -1,151 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
-using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace AtlasStarter
 {
-    class MainClass
+    class MainClassNoPrompt
     {
-        static string[] names = new string[] { "john", "paul", "george", "ringo" };
+        static string[] names = new string[] { "John", "Paul", "George", "Ringo" };
 
         public static void Main(string[] args)
         {
-            WritePrompt("Welcome! Please provide your Atlas connection string:");
-            var url = Console.ReadLine();
-            WritePrompt($"Connecting to {url}...");
+            // Provide your Atlas connection string here:
+
+            var mongoUri = "";
 
             IMongoClient client;
-            IMongoCollection<BsonDocument> collection;
+
+            // Note below that you must define the *type* of data stored in the
+            // collection. Using mapping classes is strongly advised, but if you
+            // don't create them, you can always use the more generic BsonDocument
+            // type.
+            IMongoCollection<TestDocument> collection;
 
             try
             {
-                client = new MongoClient(url);
+                client = new MongoClient(mongoUri);
             }
             catch (Exception e)
             {
-                WriteError("Uh oh. I couldn't connect. Did you provide a valid username and password?", e);
-                return;
-            }
-            WritePrompt("What's the  name of the database you want to use?");
-            var dbName = Console.ReadLine();
-            WritePrompt("What's the  name of the colletion you want to use?");
-            var collectionName = Console.ReadLine();
-
-            try
-            {
-                collection = client.GetDatabase(dbName).GetCollection<BsonDocument>(collectionName);
-            }catch (Exception e)
-            {
-                WriteError("Uh oh. I couldn't find or create the collection. Did you enter a blank value for one of the names, perhaps?", e);
                 return;
             }
 
-            WritePrompt("I'm ready to write 4 test documents now. Press any key to continue.");
-            Console.ReadKey();
+            // Provide the name of the database and collection you want to use.
+            // If they don't already exist, the driver and Atlas will create them
+            // automatically when you first write data.
+            var dbName = "";
+            var collectionName = "";
 
             try
             {
-                var docs = new List<BsonDocument>();
-                
+                collection = client.GetDatabase(dbName).GetCollection<TestDocument>(collectionName);
+            }
+            catch (Exception e)
+            {
+                return;
+            }
+
+            /*      *** INSERT ***
+             * 
+             * You can insert individual documents using collection.Insert(). 
+             * In this example, we're going to create 4 documents and then 
+             * insert them all in one call with InsertMany().
+             */
+            try
+            {
+                var docs = new List<TestDocument>();
 
                 for (int i = 0; i < 4; i++)
                 {
-                    var doc = new BsonDocument
-                    {
-                        { "name", names[i] },
-                        { "foo", i },
-                        { "info", new BsonDocument
-                            {
-                                { "x", i * 42 },
-                                { "y", i % 42 }
-                            }}
-                    };
-
+                    var doc = new TestDocument(names[i], i, i * 42, i % 42);
                     docs.Add(doc);
-
                 }
 
+                // Now that we have a collection of documents, let's write them
+                // to our Atlas data store:
                 collection.InsertMany(docs);
-
             }
             catch (Exception e)
             {
-                WriteError("Something went wrong; I couldn't insert the documents.", e);
                 return;
             }
 
-            WritePrompt("Done. Here are the documents I created:");
+            /*      *** FIND ***
+             * 
+             * Now that we have data in Atlas, we can read it. To retrieve all of
+             * the data in a collection, we call Find() with an empty filter. 
+             * The Builders class is very helpful when building complex 
+             * filters, and is used here to show its most basic use.
+             */
 
-            foreach(BsonDocument doc in collection.Find(Builders<BsonDocument>.Filter.Empty).ToList())
+            var allDocs = collection.Find(Builders<TestDocument>.Filter.Empty)
+                .ToList();
+
+            foreach (TestDocument doc in allDocs)
             {
-                Console.WriteLine(doc.ToString());
+                // Do something with the documents.
             }
-           
 
-            int id = getNumberFromUser();
+            // I will now find the first document what has a value of 3 for
+            // the Type property. Again we use the Builders class to create
+            // the filter, and a simple LINQ statement to define the property
+            // and value we're after:
 
-            var filter = Builders<BsonDocument>.Filter.Eq("foo", id);
+            var findFilter = Builders<TestDocument>.Filter.Eq(t => t.Type, 3);
 
-            var findResult = collection.Find(filter).FirstOrDefault();
+            var findResult = collection.Find(findFilter).FirstOrDefault();
 
             if (findResult == null)
             {
-                WriteError("Something went wrong; I couldn't find that document.", null);
                 return;
             }
-            WritePrompt($"I found the document with foo = {id}:");
-            Console.WriteLine($"{findResult.ToString()}.");
-            WritePrompt("I will now update the info.x value on that document.");
 
-            var update = Builders<BsonDocument>.Update.Set("info.x", 420);
-            var options = new FindOneAndUpdateOptions<BsonDocument, BsonDocument>() { ReturnDocument = ReturnDocument.After };
-            var updatedDocument = collection.FindOneAndUpdate(filter, update, options);
+            /*      *** UPDATE **
+             * 
+             * You can update a single document, as shown here, or multiple 
+             * documents in a single call.
+             * 
+             * Here we update the Coordinates.X value on the document we just found
+             */
 
-            findResult = collection.Find(filter).FirstOrDefault();
+            var updateFilter = Builders<TestDocument>.Update.Set(t => t.Coordinates.X, 420);
 
-            WritePrompt("Here's the updated document:");
-            Console.WriteLine(updatedDocument.ToString());
-            WritePrompt("Press any key to delete all the records, or Ctrl-C to quit without deleting.");
-            Console.ReadLine();
+            // These FindOneAndUpdateOptions specify that we want the *updated* document
+            // to be returned to us. By default, we get the document as it was *before*
+            // the update.
 
-            var deleteResult = collection.DeleteMany(Builders<BsonDocument>.Filter.In("name", names));
-            WritePrompt($"I deleted {deleteResult.DeletedCount} records.");
-        }
-
-        private static int getNumberFromUser()
-        {
-            WritePrompt("Type a number between 0 and 3");
-            var input = Console.ReadKey().KeyChar;
-            Console.WriteLine();
-            int id = -1;
-            int.TryParse(input.ToString(), out id);
-
-            if (id < 0 || id > 3)
+            var options = new FindOneAndUpdateOptions<TestDocument, TestDocument>()
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("That was not a valid number; please try again.");
-                Console.ForegroundColor = ConsoleColor.White;
-                return getNumberFromUser();
-            }
+                ReturnDocument = ReturnDocument.After
+            };
 
-            else return id;
+            // the updatedDocument object is a TestDocument that refelcts the
+            // changes.
+            var updatedDocument = collection.FindOneAndUpdate(findFilter,
+                updateFilter, options);
+
+            /*      *** DELETE ***
+             *      
+             *      As with other CRUD methods, you can delete a single document 
+             *      or all documents that match a specified filter. To delete all 
+             *      of the documents in a collection, pass an empty filter to 
+             *      the DeleteMany() method:
+             */
+
+            var deleteResult = collection
+                .DeleteMany(Builders<TestDocument>
+                .Filter.In("name", names));
+
+            //You can get the count of deleted records with {deleteResult.DeletedCount}
         }
 
-        private static void WritePrompt(string text)
-        {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
-            Console.WriteLine(text);
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-        private static void WriteError(string text, Exception e)
-        {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(text);
-            Console.WriteLine($"Here's the exception: {e}");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.ReadLine();
-            return;
-        }
     }
 }
---
+++ AtlasStarter/TestDocument.cs
@@ -0,0 +1,32 @@
+﻿namespace AtlasStarter
+{
+    public class TestDocument
+    {
+        public string Name { get; set; }
+        public int Type { get; set; }
+        public Coordinates Coordinates { get; set; }
+
+        public TestDocument()
+        {
+        }
+
+        public TestDocument(string name, int type, int x, int y)
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Coordinates = new Coordinates(x, y);
+        }
+    }
+
+    public class Coordinates
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Coordinates(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+}
